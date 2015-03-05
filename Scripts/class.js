c31e82c3fc8fde2f081ac595c72c290a1e18158a
Hristo Chakarov <mail@ickata.net>
2015-03-05 18:44:27 +0200

    Extend super class

--- Scripts/class.js
+++ Scripts/class.js
@@ -4,14 +4,34 @@
  *
  * Class creator
  * 
+ * @requires @function extend
+ * 
  * @param prototype {Object} The prototype object
  *
  * @returns {Function} constructor
 **/
 function Class( prototype ) {
    prototype         = prototype || {};
-   var constructor   = prototype.initialize || function () {};
-   constructor.prototype = prototype;
-   constructor.prototype.constructor = constructor;
+   var parent        = typeof prototype.Extends == 'function' ? prototype.Extends.prototype : {};
+   // create our constructor - use provided `initialize`
+   // method or create empty function
+   var constructor   = function () {
+      // get the constructor function
+      var fn               = (prototype.initialize || parent.initialize || function () {});
+      // create empty function that we will use for the real constructor
+      var Instance         = function () {};
+      // link prototypes
+      Instance.prototype   = constructor.prototype;
+      // create the new instance; `instance` will inherit the `constructor` prototype
+      var instance         = new Instance();
+      // execute the constructor function, changing the context with the `instance`
+      var return_val       = fn.apply( instance, arguments );
+      // In case the return result of the constructor fn is object, return this object.
+      // Otherwise return the `instance`.
+      return typeof( return_val ) == 'object' ? return_val : instance;
+   }
+   // set the prototype object
+   // inherit properties & methods from the parent class
+   constructor.prototype = extend( {}, parent, prototype );
    return constructor;
 }
