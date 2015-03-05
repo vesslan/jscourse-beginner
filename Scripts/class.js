47333abc90515711b11d23187d920f108fbe0477
Hristo Chakarov <mail@ickata.net>
2015-03-05 18:44:52 +0200

    Ability to call super methods

--- Scripts/class.js
+++ Scripts/class.js
@@ -33,5 +33,20 @@ function Class( prototype ) {
    // set the prototype object
    // inherit properties & methods from the parent class
    constructor.prototype = extend( {}, parent, prototype );
+   // set name to all annonymous functions
+   for ( var name in constructor.prototype ) {
+      if ( typeof( constructor.prototype[ name ] ) == 'function' ) {
+         // set the same name as the key
+         constructor.prototype[ name ].__name__ = name;
+      }
+   }
+   // make parent method calling available via `this.parent()`
+   constructor.prototype.parent = function () {
+      var fn = parent[ arguments.callee.caller.__name__ ];
+      if ( typeof fn !== 'function' ) {
+         throw new Error('Cannot call `parent`');
+      }
+      return fn.apply( this, arguments );
+   }
    return constructor;
 }
