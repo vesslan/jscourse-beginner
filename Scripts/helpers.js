b83e4616d9b4c4c4aff08aa99af86035cc45e66a
Hristo Chakarov <mail@ickata.net>
2015-03-10 17:04:32 +0200

    Missing API - Array methods

--- Scripts/helpers.js
+++ Scripts/helpers.js
@@ -83,3 +83,85 @@ function extend( obj ) {
       return fn.apply( obj, args.concat.apply( args, arguments ) );
    }
 });
+
+/**
+ * Array.prototype.forEach
+ *
+ * Loops an array and executes a callback function for every element of the array
+ *
+ * @param callback   {Function}  The callback function to be executed
+ * @param context    {Object}    The callback context (represented by `this`)
+ *
+ * @returns {Array} The same array
+**/
+! Array.prototype.forEach && (Array.prototype.forEach = function ( callback, context ) {
+   // `this` is our array
+   // perform a standard for loop
+   for ( var i=0, l=this.length; i<l; i += 1 ) {
+      // For every element of the array, call the `callback` fn applying the `context`,
+      // and pass the element, index, and the array itself as arguments.
+      callback.call( context, this[i], i, this );
+   }
+   return this;   // enable chaining
+});
+
+/**
+ * Array.prototype.filter
+ *
+ * Loops an array and executes a callback filter function for every element of the array
+ * Returns new array with filtered elements
+ * The callback fn must return truthy value if we want the element in the new array
+ *
+ * @param callback   {Function}  The callback function to be executed
+ * @param context    {Object}    The callback context (represented by `this`)
+ *
+ * @returns {Array} The same array
+**/
+! Array.prototype.filter && (Array.prototype.filter = function ( callback, context ) {
+   var filtered_arr = [];  // our new filtered array
+   // `this` is our array
+   // perform a standard for loop
+   for ( var i=0, l=this.length; i<l; i += 1 ) {
+      // For every element of the array, call the `callback` fn applying the `context`,
+      // and pass the element, index, and the array itself as arguments.
+      // Push the element into the filtered array if the `callback` returns truthy value.
+      callback.call( context, this[i], i, this ) && filtered_arr.push( this[i] );
+   }
+   return filtered_arr; // the new filtered array
+});
+
+/**
+ * Array.prototype.indexOf
+ *
+ * Returns first index of an element (if exists) inside the array, otherwise returns -1
+ *
+ * @param item {Mixed}  The element we're searching for
+ *
+ * @returns {Number}
+**/
+! Array.prototype.indexOf && (Array.prototype.indexOf = function ( item ) {
+   for ( var i=0, l=this.length; i<l; i += 1 ) {
+      if ( item === this[i] ) {
+         return i;
+      }
+   }
+   return -1;
+});
+
+/**
+ * Array.prototype.lastIndexOf
+ *
+ * Returns last index of an element (if exists) inside the array, otherwise returns -1
+ *
+ * @param item {Mixed}  The element we're searching for
+ *
+ * @returns {Number}
+**/
+! Array.prototype.lastIndexOf && (Array.prototype.lastIndexOf = function ( item ) {
+   for ( var i=this.length-1; i>=0; i -= 1 ) {
+      if ( item === this[i] ) {
+         return i;
+      }
+   }
+   return -1;
+});
